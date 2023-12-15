af136fba4848d381818d5325b4cf5f97fa1ac6c0
1plus <122733139+Chen1Plus@users.noreply.github.com>
2023-12-16 01:21:50 +0800

    13086 performance improve

--- 13086.c
+++ 13086.c
@@ -3,55 +3,53 @@
 #include <string.h>
 
 #define MAX_STR 2010
+#define MAX_N 60
 
 
 typedef struct _f_str {
     unsigned long long len;
     struct _f_str *a;
     struct _f_str *b;
-    // for f0 and f1 only, otherwise NULL
     char *str;
 } f_str;
 
 int main() {
+    int n;
+    unsigned long long k;
+    f_str *arr = malloc(sizeof(f_str) * MAX_N);
+    arr[0].str = malloc(sizeof(char) * MAX_STR);
+    arr[1].str = malloc(sizeof(char) * MAX_STR);
+    arr[0].a = NULL;
+    arr[1].a = NULL;
+
     int t;
     scanf("%d", &t);
     while (t--) {
-        int n;
-        unsigned long long k;
-        f_str *arr;
-
-        char *a = malloc(sizeof(char) * MAX_STR);
-        char *b = malloc(sizeof(char) * MAX_STR);
-        scanf("%s %s", a, b);
+        scanf("%s %s", arr[0].str, arr[1].str);
         scanf("%d %lld", &n, &k);
-        arr = malloc(sizeof(f_str) * (n >= 2 ? n + 1 : 2));
-        arr[0].len = strlen(a);
-        arr[0].str = a;
-        arr[0].a = NULL;
-        arr[1].len = strlen(b);
-        arr[1].str = b;
-        arr[1].a = NULL;
+        arr[0].len = strlen(arr[0].str);
+        arr[1].len = strlen(arr[1].str);
+
         for (int i = 2; i <= n; i++) {
             arr[i].len = arr[i - 1].len + arr[i - 2].len;
             arr[i].a = &arr[i - 2];
             arr[i].b = &arr[i - 1];
-            arr[i].str = NULL;
         }
 
         f_str *cur = &arr[n];
-        while (cur->str == NULL && cur->a != NULL)
+        while (cur->a != NULL)
             if (cur->a->len > k)
                 cur = cur->a;
             else {
                 k -= cur->a->len;
                 cur = cur->b;
             }
-        printf("%c\n", cur->str[k]);
 
-        free(arr[0].str);
-        free(arr[1].str);
-        free(arr);
+        printf("%c\n", cur->str[k]);
     }
+
+    free(arr[0].str);
+    free(arr[1].str);
+    free(arr);
     return 0;
 }
