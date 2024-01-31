2f33fd36af0cfe0759d89d67e542af0c919f98b6
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-01-31 11:44:43 +0800

    12605

---
+++ src/12605.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char from[26];
+    for (int i = 0; i < 26; i++) from[i] = (char)('a' + i);
+
+    int n, m;
+    scanf("%d %d", &n, &m);
+    char *str = malloc(sizeof(char) * (n + 1));
+    scanf("%s", str);
+
+    char *a = malloc(sizeof(char) * m);
+    char *b = malloc(sizeof(char) * m);
+    for (int i = 0; i < m; i++) scanf(" %c %c", &a[i], &b[i]);
+
+    for (int i = m - 1; i >= 0; i--) {
+        char tmp         = from[a[i] - 'a'];
+        from[a[i] - 'a'] = from[b[i] - 'a'];
+        from[b[i] - 'a'] = tmp;
+    }
+
+    for (int i = 0; i < n; i++) printf("%c", from[str[i] - 'a']);
+    printf("\n");
+}
