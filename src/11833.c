03b3659f6583aeda38cbe6d1c8dda9c4a4c3e8ac
1plus <122733139+Chen1Plus@users.noreply.github.com>
2024-03-19 12:01:55 +0800

    remove unused variable

--- src/11833.c
+++ src/11833.c
@@ -4,12 +4,12 @@ int arr_idx_from_value[200000];
 int arr[200000];
 int pre_arr[200000];
 
-void parse(int arr_l, int arr_r, int pre_l, int pre_r) {
+void parse(int arr_l, int arr_r, int pre_l) {
     if (arr_l > arr_r) return;
 
 #define mid_idx arr_idx_from_value[pre_arr[pre_l]]
-    parse(arr_l, mid_idx - 1, pre_l + 1, pre_l + mid_idx - arr_l);
-    parse(mid_idx + 1, arr_r, pre_l + mid_idx - arr_l + 1, pre_r);
+    parse(arr_l, mid_idx - 1, pre_l + 1);
+    parse(mid_idx + 1, arr_r, pre_l + mid_idx - arr_l + 1);
     printf("%d ", pre_arr[pre_l]);
 }
 
@@ -22,6 +22,6 @@ int main() {
     }
     for (int i = 0; i < n; i++) scanf("%d", &pre_arr[i]);
 
-    parse(0, n - 1, 0, n - 1);
+    parse(0, n - 1, 0);
     printf("\n");
 }
